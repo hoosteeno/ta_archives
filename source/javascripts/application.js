2456d5f4f1cce807cbe1b0c7238f5eede111c947
hoosteeno <justin@dojo4.com>
2012-09-19 16:03:43 -0600

    proof of concept multiple videos on page

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,2 +1,9 @@
 /* Application JS: */
 
+/* flowplayer config */
+  //flowplayer("a.player", "javascripts/flowplayer/flowplayer-3.2.15.swf");
+
+$(function() {
+  flowplayer("a.player", "javascripts/flowplayer/flowplayer-3.2.15.swf");
+});
+
